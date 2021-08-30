ce12b8b207d7217d495073aca0fbbc3aac772d6c
Bob <rcook2@uccs.edu>
2021-08-29 23:29:15 -0600

    Changed back to original message.

--- Week1/src/MyFirstProgram.java
+++ Week1/src/MyFirstProgram.java
@@ -3,6 +3,6 @@ public class MyFirstProgram
 {
     public static void main(String[] args)
     {
-        System.out.println("Let's change back. ");
+        System.out.println("Hello World!");
     }
 }
