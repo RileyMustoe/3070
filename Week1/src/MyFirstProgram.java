9ee28463aecd6694a4c806d0c784dd0ebf306fd6
Bob <Bob@DESKTOP-QUAKHND>
2021-08-31 20:06:09 -0600

    Reverted comment back to Hello World!

--- Week1/src/MyFirstProgram.java
+++ Week1/src/MyFirstProgram.java
@@ -3,6 +3,6 @@ public class MyFirstProgram
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World, back again!");
+        System.out.println("Hello World!");
     }
 }
