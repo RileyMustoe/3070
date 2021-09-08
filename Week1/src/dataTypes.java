81501cf6fcbbb185f77f8e3e60071b60f2df9c1f
infs3080 <44375801+infs3080@users.noreply.github.com>
2021-09-08 08:40:29 -0600

    included Type Casting

---
+++ Week1/src/dataTypes.java
@@ -0,0 +1,61 @@
+/* Double Data Type Example
+
+class dataTypes {
+	
+	public static void main(String[]args) {
+		
+		double bobsAccount;
+		bobsAccount = 10.50;
+		
+		System.out.print(bobsAccount);
+		
+	}
+}
+
+*/
+
+/*Char datatype
+
+class dataTypes {
+	
+	public static void main(String[]args) {
+		
+		char letter = 'L';
+		
+		
+		System.out.print(letter);
+		
+	}
+}
+
+*/
+
+/* String datatype
+
+class dataTypes {
+	
+	public static void main(String[]args) {
+		
+		String sentence = "This is my sentence !! ";
+		
+		
+		System.out.print(sentence);
+		
+	}
+}
+
+*/
+
+// Boolean dataType
+
+class dataTypes {
+	
+	public static void main(String[]args) {
+		
+		boolean winner = true;
+		
+		
+		System.out.print(winner);
+		
+	}
+}
