26f940af274b0e2dc56634b28000066fd1528fb4
infs3080 <44375801+infs3080@users.noreply.github.com>
2021-09-08 08:17:40 -0600

    Variable Example

---
+++ Week1/src/variable1.java
@@ -0,0 +1,16 @@
+// Initial Variable example
+
+class variable1 {
+	
+	public static void main(String[] args) {
+		
+		int bobsAccount;
+		bobsAccount = 10;
+		
+		System.out.print(bobsAccount);
+		
+	}
+	
+}
+
+
