9cb32bf6ea0da23f584f2507230fa7315bb6a075
Bob <rcook2@uccs.edu>
2021-09-13 19:53:15 -0600

    Reverted First Program

--- Week1/src/MyFirstProgram.java
+++ Week1/src/MyFirstProgram.java
@@ -3,7 +3,6 @@ public class MyFirstProgram
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World, it's a nice day!");
+        System.out.println("Hello World!");
     }
 }
- 
