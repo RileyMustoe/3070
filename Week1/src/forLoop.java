43076f1ba1cb0c0dc76a811b0c1dba954ebeb594
infs3080 <44375801+infs3080@users.noreply.github.com>
2021-09-22 10:02:34 -0600

    Added Break and Continue

---
+++ Week1/src/forLoop.java
@@ -0,0 +1,31 @@
+
+public class forLoop {
+
+	public static void main(String[] args) {
+
+		/*
+		// Example if incrementing For Loop
+		for (int i = 0; i <=5; i++) {
+			System.out.println(i);
+		}
+		*/
+		
+		/*
+		// Countdown example
+		for (int i = 10; i>=0; i--) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Kaboom!!!!");
+		*/
+		
+		for (int i = 10; i >=0; i-=2) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Kaboom!!!");
+		
+		
+	}
+
+}
