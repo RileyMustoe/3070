105193ff4e9314196e8d3206c8a3be8e7476e6f6
COOK-INFS <44375801+COOK-INFS@users.noreply.github.com>
2021-09-29 17:00:57 -0600

    Changed name from Week1 to Examples.
    Changed name from Week1 to Examples.

---
+++ Examples/src/operators.java
@@ -0,0 +1,16 @@
+//Operators function example
+
+public class operators {
+	
+	public static void main(String[]args) {
+		
+		int a = 10;
+		int b = 10;
+		
+		int c = a+b;
+		
+		System.out.print(c);
+		
+		
+	}
+}
